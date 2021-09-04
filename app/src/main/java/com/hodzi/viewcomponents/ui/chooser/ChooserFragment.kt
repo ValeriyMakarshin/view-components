b5a215989839ec8be3f53028fa7290ab8e252e5d
v.makarshin <v.makarshin@corp.vk.com>
2021-09-04 18:17:34 +0300

    Added new fragment

---
+++ app/src/main/java/com/hodzi/viewcomponents/ui/chooser/ChooserFragment.kt
@@ -0,0 +1,16 @@
+package com.hodzi.viewcomponents.ui.chooser
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.hodzi.viewcomponents.R
+
+class ChooserFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        return inflater.inflate(R.layout.fragment_progress, container, false)
+    }
+}
