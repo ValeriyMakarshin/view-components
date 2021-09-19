3343e70da7d43f803c0ba83d832664d7c1962705
v.makarshin <v.makarshin@corp.vk.com>
2021-09-20 01:14:48 +0300

    Added view pool

--- app/src/main/java/com/hodzi/viewcomponents/ui/chooser/ChooserFragment.kt
+++ app/src/main/java/com/hodzi/viewcomponents/ui/chooser/ChooserFragment.kt
@@ -1,16 +1,46 @@
 package com.hodzi.viewcomponents.ui.chooser
 
+import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.hodzi.viewcomponents.R
+import com.hodzi.viewcomponents.ui.chooser.view.ChooserView
 
 class ChooserFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         super.onCreateView(inflater, container, savedInstanceState)
         return inflater.inflate(R.layout.fragment_chooser, container, false)
+    }
+
+    private val colorPool = listOf(Color.BLUE, Color.RED, Color.GREEN)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val chooserView = view.findViewById<ChooserView>(R.id.chooser_view)
+        var index = 0
+        chooserView.setSwipeView(getView(requireContext(), index))
+        chooserView.setNextView(getView(requireContext(), ++index))
+        chooserView.setSwipeListener {
+            chooserView.setSwipeView(getView(requireContext(), index))
+            chooserView.setNextView(getView(requireContext(), ++index))
+        }
+    }
+
+    // todo: use viewHolder
+    private fun getView(context: Context, index: Int): View {
+        val textView = TextView(context)
+        textView.layoutParams = ViewGroup.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.MATCH_PARENT
+        )
+        textView.setBackgroundColor(colorPool[index % colorPool.size])
+        textView.text = index.toString()
+        return textView
     }
 }
--- app/src/main/java/com/hodzi/viewcomponents/ui/chooser/view/ChooserView.kt
+++ app/src/main/java/com/hodzi/viewcomponents/ui/chooser/view/ChooserView.kt
@@ -2,18 +2,24 @@ package com.hodzi.viewcomponents.ui.chooser.view
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.View
+import android.view.ViewGroup
 import android.widget.FrameLayout
 import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.constraintlayout.motion.widget.TransitionAdapter
 import com.hodzi.viewcomponents.R
 
 
-// SwipeLayout ?
 class ChooserView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0,
 ) : FrameLayout(context, attrs, defStyleAttr) {
+    private var onSwipe: (() -> Unit)? = null
+
+    private val nextContainer: ViewGroup
+    private val swipeContainer: ViewGroup
+
     init {
         val view = inflate(getContext(), R.layout.view_chooser, this)
         val motionLayout = view.findViewById<MotionLayout>(R.id.motion_layout)
@@ -22,9 +28,26 @@ class ChooserView @JvmOverloads constructor(
                 if (currentId in listOf(R.id.like_finish, R.id.dislike_finish) && p0.progress == 1f) {
                     p0.setTransition(R.id.start, R.id.like)
                     p0.progress = 0f
+                    onSwipe?.invoke()
                 }
             }
         })
+        nextContainer = view.findViewById(R.id.next_layout)
+        swipeContainer = view.findViewById(R.id.swipe_layout)
+    }
+
+    fun setSwipeListener(onSwipe: () -> Unit) {
+        this.onSwipe = onSwipe
+    }
+
+    fun setNextView(nextView: View) {
+        nextContainer.removeAllViews()
+        nextContainer.addView(nextView)
+    }
+
+    fun setSwipeView(swipeView: View) {
+        swipeContainer.removeAllViews()
+        swipeContainer.addView(swipeView)
     }
 
     companion object {
